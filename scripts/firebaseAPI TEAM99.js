cc5861752d6e2697e9935ab2ddfede05b32d9a2a
Waleed Ur Rehman <waleedurrehman12@gmail.com>
2021-11-09 10:44:18 -0800

    Initial Commit

---
+++ scripts/firebaseAPI TEAM99.js
@@ -0,0 +1,22 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAPocUih2WcCtDLusYu_-4hSt3g8cU5DlI",
+
+  authDomain: "demo08-waleed.firebaseapp.com",
+
+  projectId: "demo08-waleed",
+
+  storageBucket: "demo08-waleed.appspot.com",
+
+  messagingSenderId: "376805407256",
+
+  appId: "1:376805407256:web:7a5a0b0f605d76f9ad216d"
+
+};
+
+  
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
